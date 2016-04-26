6fa203dc7525c3f3f0d1e6f9f50d631c526d5f4d
Markus Feldbacher <m.feldbacher@student.tugraz.at>
2016-04-26 17:18:41 +0200

    [MF] Implemented a first version of a dynamic main menu layout

--- app/src/main/java/com/hyperion/dashdroid/MainActivity.java
+++ app/src/main/java/com/hyperion/dashdroid/MainActivity.java
@@ -1,15 +1,41 @@
 package com.hyperion.dashdroid;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // FIXME: Just for testing
+    GridView mGrid;
+    String[] mTileText = {"Radio", "Wetter", "TV", "RSS", "NEWS"};
+    int[] mIconId = {R.drawable.ic_radio_black_48dp,
+            R.drawable.ic_wb_sunny_black_48dp,
+            R.drawable.ic_tv_black_48dp,
+            R.drawable.ic_rss_feed_black_48dp,
+            R.drawable.ic_today_black_48dp};
+    int[] mTileWidth = {1, 1, 2, 1, 1};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-
+        // FIXME: Just for testing
+        MainMenuGrid adapter = new MainMenuGrid(MainActivity.this, mTileText, mIconId);
+        mGrid = (GridView) findViewById(R.id.grid);
+        mGrid.setAdapter(adapter);
     }
 }
--- app/src/test/java/com/hyperion/dashdroid/MainLayoutUnitTest.java
+++ app/src/test/java/com/hyperion/dashdroid/MainLayoutUnitTest.java
@@ -1,13 +1,15 @@
 package com.hyperion.dashdroid;
 
+import android.test.suitebuilder.annotation.SmallTest;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
+@SmallTest
+public class MainLayoutUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
