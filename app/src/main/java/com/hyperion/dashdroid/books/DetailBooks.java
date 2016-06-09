df3c8901027ccc7f629601d48b93e0c5d486ae3b
Valdrin Maloku <v.maloku@student.tugraz.at>
2016-06-09 09:51:15 +0200

    [RH, VM] - Working on Books Module and Books detail

---
+++ app/src/main/java/com/hyperion/dashdroid/books/DetailBooks.java
@@ -0,0 +1,18 @@
+package com.hyperion.dashdroid.books;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hyperion.dashdroid.R;
+
+/**
+ * Created by Valdrin on 6/8/2016.
+ */
+public class DetailBooks extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.books_fragment_detail);
+    }
+}
